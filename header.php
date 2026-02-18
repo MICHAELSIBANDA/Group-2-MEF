0d6c209a1beec5b1bd1a20dcac2ca80ccdce1da7
F Mwenje <224256108@tut4life.ac.za>
2026-02-18 15:59:47 +0200

    Initial commit - Make Education Fashionable website

---
+++ header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Make Education Fashionable</title>
+
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;700&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+        <link rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+            integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4B6Q2f4qz7z9M+YyF2Gg=="
+            crossorigin="anonymous"
+            referrerpolicy="no-referrer" 
+        />
+
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <div class="site-bg"></div>
+
+       <header class="navbar">
+            <div class="container nav-content">
+                <a href="#home" class="logo">
+                    <img src="images/logos.png" alt="Make Education Fashionable">
+                </a>
+                <nav>
+                    <a href="#home">Home</a>
+                    <a href="#about">About</a>
+                </nav>
+            </div>
+        </header>
+
+<main>
+
+<main>
